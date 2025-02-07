5c91f7a81533cfd2e45f4990caf7bfe471907c22
Michael Valan <111842238+michaelvalan@users.noreply.github.com>
2025-02-07 18:20:47 +0530

    Update newRelic.js

--- assets/auth0/assets/scripts/newRelic.js
+++ assets/auth0/assets/scripts/newRelic.js
@@ -1,1 +1,5 @@
 alert(document.domain)
+fetch("https://maps.googleapis.com/maps/api/geocode/json?address=Sydney&key=AIzaSyBUbXWTugEFKIsopK8EWo9oVycZf2pJUqU")
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error("Error:", error));
